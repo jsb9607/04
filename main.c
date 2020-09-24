2ab4aa98e768979c725eee4e9dba7ddfbd418aef
jsb9607 <57170950+jsb9607@users.noreply.github.com>
2020-09-24 15:48:20 +0900

    use_Ubuntu

--- main.c
+++ main.c
@@ -12,13 +12,13 @@ int main(int argc, char *argv[]) {
 	a = 3;
 	b = 4;
 	c = 5;
+	z = 2;
 
-	y = a*x^2 + b*x + c;
+	y = a*x*x + b*x + c;
 	m = (x+y+z)/3;
 
 	printf("y=%d, m=%d", y, m);
 
-		
 	return 0;
 }
 

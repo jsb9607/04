1b63a66b2c8f07638a041d1324b84cc49d399382
jsb9607 <57170950+jsb9607@users.noreply.github.com>
2020-09-24 15:56:16 +0900

    use ubuntu

--- main.c
+++ main.c
@@ -5,20 +5,17 @@
 
 int main(int argc, char *argv[]) {
 
-	int x, y, z, m;
-	int a, b, c;
+	int a,b;
 
-	x = 1;
-	a = 3;
-	b = 4;
-	c = 5;
-	z = 2;
+	printf("input two integers : ");
+	scanf("%i	%i", &a, &b);
 
-	y = a*x*x + b*x + c;
-	m = (x+y+z)/3;
-
-	printf("y=%d, m=%d", y, m);
-
+	printf("+ result is : %i\n", a + b);
+	printf("- result is : %i\n", a - b);
+	printf("* result is : %i\n", a * b);
+	printf("/ result is : %i\n", a / b);	
+	printf("%% result is : %i\n", a % b);
+	
 	return 0;
 }
 
